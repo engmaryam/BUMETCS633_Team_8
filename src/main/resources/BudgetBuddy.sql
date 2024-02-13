ab1e0476b716a0efc892a346d20b97fe9edb8179
Jonathan Duran <duranxci@gmail.com>
2024-02-12 20:06:09 -0500

    mysql initial tables update

--- src/main/resources/BudgetBuddy.sql
+++ src/main/resources/BudgetBuddy.sql
@@ -6,7 +6,7 @@ USE budget_buddy_app;
 
 -- Table for User
 CREATE TABLE IF NOT EXISTS users (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
@@ -15,22 +15,22 @@ CREATE TABLE IF NOT EXISTS users (
 
 -- Table for Income
 CREATE TABLE IF NOT EXISTS income (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     amount DECIMAL(10, 2) NOT NULL,
     source VARCHAR(255) NOT NULL,
     date DATE NOT NULL,
     -- Add other income-related fields as needed
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 -- Table for Expenses
 CREATE TABLE IF NOT EXISTS expenses (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
     amount DECIMAL(10, 2) NOT NULL,
     category VARCHAR(255) NOT NULL,
     date DATE NOT NULL,
     -- Add other expense-related fields as needed
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
