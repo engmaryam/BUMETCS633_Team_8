e940b4031e6226f87f0ddcd4777a807c2b5d0e12
Jonathan Duran <duranxci@gmail.com>
2024-02-15 20:22:34 -0500

    Update BudgetBuddy.sql

--- src/main/resources/BudgetBuddy.sql
+++ src/main/resources/BudgetBuddy.sql
@@ -5,32 +5,32 @@ CREATE DATABASE IF NOT EXISTS budget_buddy_app;
 USE budget_buddy_app;
 
 -- Table for User
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS user(
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL
-    -- Add other user-related fields as needed
 );
 
 -- Table for Income
 CREATE TABLE IF NOT EXISTS income (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
+id INT AUTO_INCREMENT PRIMARY KEY, 
+   user_id INT,
     amount DECIMAL(10, 2) NOT NULL,
     source VARCHAR(255) NOT NULL,
     date DATE NOT NULL,
     -- Add other income-related fields as needed
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES user(id)
 );
+
 
 -- Table for Expenses
 CREATE TABLE IF NOT EXISTS expenses (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+id INT AUTO_INCREMENT PRIMARY KEY, 
     user_id INT,
     amount DECIMAL(10, 2) NOT NULL,
     category VARCHAR(255) NOT NULL,
     date DATE NOT NULL,
     -- Add other expense-related fields as needed
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES user(id)
 );
